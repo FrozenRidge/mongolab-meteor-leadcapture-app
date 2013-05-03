1e88b19c3bdba7f4fcef6dade1609042750b327a
Niall O'Higgins <niallo@unworkable.org>
2013-05-03 11:22:53 -0700

    not quite right

--- app/app.js
+++ app/app.js
@@ -2,7 +2,24 @@ Emails = new Meteor.Collection("emails")
 
 EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+// Github account usernames of admin users
+var ADMIN_USERS = ['niallo', 'peterbraden'];
+
+function isAdmin() {
+  try {
+    console.dir(Meteor.user())
+    return Meteor.user().username in ADMIN_USERS;
+  } catch(e) {
+    console.log("exception: " + e);
+    return false;
+  }
+}
+
 if (Meteor.isClient) {
+  Meteor.autosubscribe(function() {
+    Meteor.subscribe('userData', null, function() { console.log("foo") });
+    Meteor.subscribe('allUserData', null, function() { console.log("foo") });
+  })
   Template.footer.events({
     'click .login' : function(evt, tmpl){
       console.log("Login")
@@ -36,16 +53,23 @@ if (Meteor.isClient) {
     return Session.get("emailSubmitted");
   };
 
+  Template.footer.isAdmin = isAdmin;
+
   Template.admin
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    console.log("Current Emails:");
-    Emails.find().forEach(function(email){
-      console.log(' - ', email.email, email.referrer);
-    })
-
-
+  Meteor.publish("userData", function () {
+    console.log("userData")
+    return Meteor.users.find({_id: this.userId},
+        {fields: {'services.github.username': 1, 'username':1}});
   });
+  Meteor.publish("allUserData", function () {
+    console.log("alluserData")
+    return Meteor.users.find({_id: this.userId},
+        {fields: {'services.github.username': 1, 'username':1}});
+  });
+  Meteor.startup(function () {
+  });
+
 }
